3cc4f15afe2efabe9849c35f46e3826a574c8647
RicardoWebProject <luisrijhn09@gmail.com>
2020-10-05 16:22:37 -0600

    Exportación de los modelos al sistema

---
+++ tienda/backend/models/__init__.py
@@ -0,0 +1,6 @@
+from .usuarios import Usuario
+from .categorias import Categoria
+from .articulos import Articulo
+from .clienteProveedores import Cliente, Fabrica
+from .direcciones import Direcciones
+from .pedidos import Pedido

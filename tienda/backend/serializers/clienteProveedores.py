e628868ab751bc36f86c3f326ec177b035a0bfd3
RicardoWebProject <luisrijhn09@gmail.com>
2020-10-19 18:38:17 -0600

    Modificado el serializador de clientes

--- tienda/backend/serializers/clienteProveedores.py
+++ tienda/backend/serializers/clienteProveedores.py
@@ -6,18 +6,16 @@ from ..models.direcciones import Direcciones
 from .direcciones import DireccionesSerializer
 
 class ClienteSerializer (serializers.ModelSerializer):
-    direcciones = DireccionesSerializer(many = True)
-    
     class Meta:
         model = Cliente
-        fields = ['id', 'nombre', 'saldo', 'credito_limite', 'direcciones']
+        fields = ['id', 'nombre', 'saldo', 'credito_limite', 'no_calle', 'comuna', 'ciudad']
     
-    def create(self, validated_data):
-        direcciones_data = validated_data.pop('direcciones')
-        cliente = Cliente.objects.create(**validated_data)
-        for direccion_data in direccion_data:
-            Direcciones.objects.create(cliente = cliente, **direccion_data)
-        return cliente
+    # def create(self, validated_data):
+    #     direcciones_data = validated_data.pop('direcciones')
+    #     cliente = Cliente.objects.create(**validated_data)
+    #     for direccion_data in direccion_data:
+    #         Direcciones.objects.create(cliente = cliente, **direccion_data)
+    #     return cliente
 
 class FabricaSerializer (serializers.ModelSerializer):
     class Meta:
